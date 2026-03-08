68fce03f347ab83196d33d8665678acbf78fd2c4
Jeem <bushrajeem32@gmail.com>
2026-03-09 01:04:23 +0600

    Initial project structure: Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className=' text-2xl p-10 font-bold text-center'>
+      AdFlow Network
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      This is where all the home page or landing page content will go.
+    </div>
+  )
+}
+
+export default Home
